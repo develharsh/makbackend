0524c0a5063a1ab40104cba722c8fab1e9cc5e7b
cpbhai <harshvsingh.223@gmail.com>
2021-11-12 04:01:47 +0530

    prod 1.0.8

--- app.js
+++ app.js
@@ -14,14 +14,6 @@ app.use(
 );
 app.use(fileUpload());
 app.use(cookieParser());
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  next();
-});
 
 //Router Imports
 app.use("/api/v1/team", require("./routes/teamRoute.js"));
