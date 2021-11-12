c3b81a3eb164f06d125ae9aa95d80bd94c4cdbcd
cpbhai <harshvsingh.223@gmail.com>
2021-11-12 19:30:22 +0530

    prod 1.0.30

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(fileUpload());
 app.use(cookieParser());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Headers", "Origin, Content-Type");
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Methods",
