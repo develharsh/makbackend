57f48f49343fe065001b68e059ddca1b193a4d86
cpbhai <harshvsingh.223@gmail.com>
2021-11-13 03:16:59 +0530

    prod 1.0.44

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const fileUpload = require("express-fileupload");
 require("dotenv").config({ path: "config/.env" });
 //const cloudinary = require("cloudinary");
 const path = require("path");
+const proxy = require("express-http-proxy");
 
 app.use(express.json());
 app.use(
@@ -14,6 +15,7 @@ app.use(
 );
 app.use(fileUpload());
 app.use(cookieParser());
+app.use("/api", proxy("https://makbackend.herokuapp.com"));
 
 //Router Imports
 app.use("/t", function (req, res, next) {
