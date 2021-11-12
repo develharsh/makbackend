6e849df68b3d3e4cf7fae7342aae32f3861e8ce9
cpbhai <harshvsingh.223@gmail.com>
2021-11-12 18:44:23 +0530

    prod 1.0.24

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Headers", "Authorization");
   res.header("Access-Control-Allow-Origin", "https://myui-kappa.vercel.app");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   next();
 });
 
