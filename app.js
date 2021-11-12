3c8ec88488418c77a8424bc01c55d95c1fb6919b
cpbhai <harshvsingh.223@gmail.com>
2021-11-12 19:28:32 +0530

    prod 1.0.29

--- app.js
+++ app.js
@@ -18,10 +18,10 @@ app.use(cookieParser());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Headers", "Content-Type");
   res.header("Access-Control-Allow-Origin", "*");
-  // res.header(
-  //   "Access-Control-Allow-Methods",
-  //   "GET, POST, PUT, DELETE, HEAD, OPTIONS"
-  // );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, HEAD, OPTIONS"
+  );
   next();
 });
 
